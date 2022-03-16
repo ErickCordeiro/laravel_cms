125fe1341db77d6fbeb0ea6018bbaf0bfeed70fc
Erick Cordeiro <erickcordeiroa@gmail.com>
2022-03-16 18:26:04 -0300

    Configuração Layout Front End Projeto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Page;
+use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +27,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Correção Bootstrap
         Paginator::useBootstrap();
+
+        //Menu
+        $frontMenu = [
+            '/' => 'Home'
+        ];
+
+        $pages = Page::all();
+        foreach ($pages as $page) {
+            $frontMenu[$page["slug"]] = $page["title"];
+        }
+
+        View::share('front_menu', $frontMenu);
+
+        //Configurações
+        $config = Setting::first();
+        View::share('front_config', $config);
     }
 }
